ecdc985708542ce825ea9eb43d63f6080b312992
systemvaz <systemvaz@outlook.com>
2022-08-07 19:36:30 +0800

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+    {
+        title: "Project 1",
+        subtitle: "subtitle here",
+        description: "description of project goes here",
+        image: "./project-imsge1.jpg",
+        link: "github.com/systemvaz",
+    },
+    {
+        title: "Project 2",
+        subtitle: "subtitle here",
+        description: "description of project 2 here",
+        image: "./project-image-2.jph",
+        link: "github.com/systemvaz",
+    },
+
+];
+
+export const skills = [
+    {
+        type: "Language",
+        strongest: "Python",
+        intermediate: "Java, JavaScript",
+        exposure: "C++, C, HTML, CSS",
+    },
+    {
+        type: "Cloud",
+        strongest: "AWS",
+        intermediate: "Azure",
+        exposure: "GCP",
+    },
+    {
+        type: "Frameworks",
+        strongest: "Flask",
+        intermediate: "TensorFlow",
+        exposure: "NodeJS",
+    },
+    {
+        type: "Methodologies",
+        strongest: "Waterfall",
+        intermediate: "Scrum/Agile",
+        exposure: "DevOps",
+    },
+];
